9c97fd6408ab53cefbbb3659a736ae843a0ae096
Olivia <oliviavidalvelazquez@gmail.com>
2022-12-15 13:01:16 +0100

    Jusqu'à 1.2

--- neurons.py
+++ neurons.py
@@ -1,1 +1,114 @@
 import numpy as np
+import pandas as pd
+
+##### Échantillons d'entraînement ######
+
+data_read = pd.read_csv("train.csv")
+
+data = np.array(data_read)
+
+np.random.shuffle(data)
+
+X_dev = data[:1000, 1:] # 1000 premieres images du tableau avec les classes
+
+Y_dev = data[:1000,0] # 1000 premieres classes du tableau associées
+
+X_train = data[1000:, 1:] # Images et classes d'entraînement
+
+Y_train = data[1000:,0] # Classes d'entraînement
+
+# Normalisation : 
+
+maxi = X_train.max()
+X_dev = X_dev/maxi
+X_train = X_train/maxi
+
+
+##### INITIALISATION #####
+
+def initialise(npixel, ncouche, nsortie):
+	"""
+	npixel = np.shape(X_dev[0]) # 784 pixels
+	ncouche = 10
+	nsortie = 10
+	"""
+
+	W0 = np.random.uniform(-0.5, 0.5, size=(npixel,ncouche))
+	W1 = np.random.uniform(-0.5, 0.5, size=(ncouche,nsortie))
+	b0 = np.random.uniform(-0.5, 0.5, ncouche)
+	b1 = np.random.uniform(-0.5, 0.5, nsortie)
+	
+	return W0, W1, b0, b1
+	
+	
+def relu(Z):
+	
+	return (Z > 0)*Z
+	
+	
+def der_relu(Z):
+	return Z > 0
+	
+def softmax(Z):
+	
+	sigma = np.exp(Z)/sum(np.exp(Z))
+	
+	return sigma
+	
+def prop_avant(data, poids):
+	"""
+	data : vecteur data pour une seule image, on va le faire pour chaque image
+	poids = poids et biais [W0, W1, b0, b1]
+	"""
+
+	Z0 = np.dot(np.transpose(poids[0]), data) + poids[2]
+	A0 = relu(Z0)
+	Z1 = np.dot(np.transpose(poids[1]), A0) + poids[3]
+	A1 = softmax(Z1)
+	
+	return Z0, A0, Z1, A1
+	
+def classe(label):
+
+	Y = np.zeros(10)
+	Y[label] = 1
+	
+	return Y
+	
+
+def prop_arriere(label, Z0, A0, Z1, A1, data, poids):
+	
+	"""
+	data : vecteur data (les pixels) pour une seule image, on va le faire pour chaque image
+	poids = poids et biais [W0, W1, b0, b1]
+	"""
+	Y = classe(label)
+	
+	delta1 = A1 - Y
+	
+	delta0 = der_relu(Z0) * np.dot(poids[1],delta1)
+	
+	dJdW0 = np.dot(data[:,None], delta0[None,:])
+	dJdW1 = np.dot(A0[:,None], delta1[None,:])
+	
+	dJdb0 = delta0
+	dJdb1 = delta1
+	
+	return dJdW0, dJdb0, dJdW1, dJdb1
+
+
+
+def actualisation(label, Z0, A0, Z1, A1, data, poids):
+	
+	dJdW0, dJdb0, dJdW1, dJdb1 = prop_arriere(label, Z0, A0, Z1, A1, data, poids)
+	dJdW0_L = [dJdW0]
+	dJdb0_L = [dJdb0]
+	dJdW1_L = [dJdW1]
+	dJdb1_L = [dJdb1]
+	
+	
+	return W0, W1, b0, b1
+
+
+
+	
--- tests.py
+++ tests.py
@@ -1,3 +1,87 @@
 import neurons as n
 import numpy as np
 import matplotlib.pyplot as plt
+
+path2plot = "plots/"
+
+print("Dimension de data : ", np.shape(n.data)) # 785, premier indice c'est la classe et les 784 autres sont les pixels de chaque image. 42000 images
+
+### TEST FONCTIONS INITIALISATION ###
+
+npixel = n.X_dev[0].size # 784 pixels
+ncouche = 10
+nsortie = 10
+
+W0, W1, b0, b1 = n.initialise(npixel, ncouche, nsortie)
+print("Shape matrice poids W0", np.shape(W0), " elle doit être (784, 10)")
+print("Shape matrice poids W1", np.shape(W1), " elle doit être (10, 10)")
+print("Shape matrice biais b0", np.shape(b0), " elle doit être (10,)")
+print("Shape matrice biais b1", np.shape(b1), " elle doit être (10,)")
+
+Z = np.arange(-10,10,1)
+
+## RELU
+
+fig, ax = plt.subplots()
+ax.plot(Z, n.relu(Z), c='b', ls='-', label="relu")
+
+ax.set_xlabel("Z")
+ax.set_ylabel("relu")
+#ax.set_title("Profil amas sans structure qui fusionne")
+plotnom = path2plot + 'relu.pdf'
+ax.legend()
+plt.savefig(plotnom)
+#plt.show()
+
+## Derivée RELU
+
+fig, ax = plt.subplots()
+ax.plot(Z, n.der_relu(Z), c='b', ls='-', label="der_relu")
+
+ax.set_xlabel("Z")
+ax.set_ylabel("der_relu")
+#ax.set_title("Profil amas sans structure qui fusionne")
+plotnom = path2plot + 'der_relu.pdf'
+ax.legend()
+plt.savefig(plotnom)
+#plt.show()
+
+## SIGMA
+
+fig, ax = plt.subplots()
+ax.plot(Z, n.softmax(Z), c='b', ls='-', label="softmax")
+
+ax.set_xlabel("Z")
+ax.set_ylabel("softmax")
+#ax.set_title("Profil amas sans structure qui fusionne")
+plotnom = path2plot + 'softmax.pdf'
+ax.legend()
+plt.savefig(plotnom)
+#plt.show()
+
+## Propagation avant
+poids = [W0, W1, b0, b1]
+Z0, A0, Z1, A1 = n.prop_avant(n.X_dev[0], poids) # X_dev[0] pour tester juste la première image.
+print("Shape matrice Z0", np.shape(Z0), " elle doit être (10,)")
+print("Shape matrice A0", np.shape(A0), " elle doit être (10,)")
+print("Shape matrice Z1", np.shape(Z1), " elle doit être (10,)")
+print("Shape matrice A1", np.shape(A1), " elle doit être (10,)")
+
+## Test Classe:
+
+label = 7
+print("Classe correspondant au chiffre", label, "est :", n.classe(7))
+
+## Propagation arriere
+
+dJdW0, dJdb0, dJdW1, dJdb1 = n.prop_arriere(label, Z0, A0, Z1, A1, n.X_dev[0], poids) # X_dev[0] pour tester juste la première image.
+print("Shape matrice dJdW0", np.shape(dJdW0), " elle doit être (784,10)")
+print("Shape matrice dJdb0", np.shape(dJdb0), " elle doit être (10,)")
+print("Shape matrice dJdW1", np.shape(dJdW1), " elle doit être (10,10)")
+print("Shape matrice dJdb1", np.shape(dJdb1), " elle doit être (10,)")
+
+
+
+
+
+
