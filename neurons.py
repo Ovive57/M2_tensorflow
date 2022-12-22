7d48150e06e7d74559e0ccab3affbbdbd05c4318
Olivia <oliviavidalvelazquez@gmail.com>
2022-12-22 21:15:52 +0100

    Netoyage finale premiere partie

--- neurons.py
+++ neurons.py
@@ -17,7 +17,7 @@ X_train = data[1000:, 1:] # Images d'entraînement
 
 Y_train = data[1000:,0] # Classes d'entraînement
 
-# Normalisation : 
+# Normalisation :
 
 maxi = X_train.max()
 X_dev = X_dev/maxi
@@ -27,112 +27,151 @@ X_train = X_train/maxi
 ##### INITIALISATION #####
 
 def initialise(npixel, ncouche, nsortie):
-	"""
+    """
     Initialisation des poids et des biais
-    Args:
-        npixel = np.shape(X_dev[0]) # 784 pixels
-        ncouche = 10
-        nsortie = 10
-    Returns:
-        W0 = poids des entrées à la couche cachée
-        W1 = poids de la couche cachée à la sortie
-        b0 = biais de la couche cachée
-        b1 = biais de la couche de sortie
-	"""
 
-	W0 = np.random.uniform(-0.5, 0.5, size=(npixel,ncouche))
-	W1 = np.random.uniform(-0.5, 0.5, size=(ncouche,nsortie))
-	b0 = np.random.uniform(-0.5, 0.5, ncouche)
-	b1 = np.random.uniform(-0.5, 0.5, nsortie)
-	
-	return W0, W1, b0, b1
-	
-	
+    Args:
+        npixel (array): _description_
+        ncouche (int): nombre de neurones dans la couche cachée
+        nsortie (int): nombre de neurones dans la couche de sortie
+
+    Returns:
+        W0 (int): poids des entrées à la couche cachée
+        W1 (int): poids de la couche cachée à la sortie
+        b0 (int): biais de la couche cachée
+        b1 (int): biais de la couche de sortie
+    """
+    W0 = np.random.uniform(-0.5, 0.5, size=(npixel,ncouche))
+    W1 = np.random.uniform(-0.5, 0.5, size=(ncouche,nsortie))
+    b0 = np.random.uniform(-0.5, 0.5, ncouche)
+    b1 = np.random.uniform(-0.5, 0.5, nsortie)
+
+    return W0, W1, b0, b1
+
 def relu(Z):
     """
     Fonction d'activation de la couche cachée
+
+    Args:
+        Z (array): variable de la fonction ReLu
+
+    Returns:
+        array: Fonction ReLu
     """
     return (Z > 0)*Z
-	
-	
+
+
 def der_relu(Z):
     """
     Dérivée de la fonction d'activation de la couche cachée
+
+    Args:
+        Z (array): variable de la fonction ReLu
+
+    Returns:
+        array: Derivée fonction ReLu
     """
     return Z > 0
-	
+
 def softmax(Z):
-	"""
-    Fonction d'activation de la couche de sortie
     """
-	sigma = np.exp(Z)/sum(np.exp(Z))
-	
-	return sigma
+    Fonction d'activation de la couche de sortie
+
+    Args:
+        Z (array): variable de la fonction Softmax
+
+    Returns:
+        array: Fonction Softmax
+    """
+    sigma = np.exp(Z)/sum(np.exp(Z))
+
+    return sigma
+
 
 ### Propagation avant ###
 
-
 def prop_avant(data, poids):
-	"""
+    """
     Propagation avant d'une image
-    Args:
-        data = vecteur contenant les pixels d'une image
-        poids = poids et biais [W0, W1, b0, b1]
-    Returns:
-        A0, A1 = vecteurs d'activation de la couche cachée/de sortie
-        Z0, Z1 = vecteurs des arguments de la couche cachée/de sortie
-	"""
 
-	Z0 = np.dot(np.transpose(poids[0]), data) + poids[2]
-	A0 = relu(Z0)
-	Z1 = np.dot(np.transpose(poids[1]), A0) + poids[3]
-	A1 = softmax(Z1)
-	
-	return Z0, A0, Z1, A1
-	
+    Args:
+        data (array): vecteur contenant les pixels d'une image
+        poids (array): vecteur contenant les poids et biais [W0, W1, b0, b1]
+
+    Returns:
+        array:
+        A0, A1 : vecteurs d'activation de la couche cachée/de sortie
+        Z0, Z1 : vecteurs des arguments de la couche cachée/de sortie
+    """
+    Z0 = np.dot(np.transpose(poids[0]), data) + poids[2]
+    A0 = relu(Z0)
+    Z1 = np.dot(np.transpose(poids[1]), A0) + poids[3]
+    A1 = softmax(Z1)
+
+    return Z0, A0, Z1, A1
+
 ### Propagation arriere ###
 
 def classe(label):
-    """
-    Ecris un label (int) sous forme binaire: vecteur Y de taille 10
+    """Ecris une classe sous forme binaire
+
+    Args:
+        label (int): classe, numéro entre 0 et 9
+
+    Returns:
+        array: vecteur de taille 10
     """
     Y = np.zeros(10)
     Y[label] = 1
     return Y
-	
+
 def prop_arriere(label, Z0, A0, Z1, A1, data, poids):
     """
     Propagation arrière pour 1 image
+
     Args:
-        label = classe associée à l'image 
-        A0, A1 = vecteurs d'activation de la couche cachée/de sortie
-        Z0, Z1 = vecteurs des arguments de la couche cachée/de sortie
-        data = vecteur data (les pixels) pour une seule image, on va le faire pour chaque image
-        poids = poids et biais [W0, W1, b0, b1]
+        label (int): classe associée à l'image
+        Z0 (array): vecteur des arguments de la couche cachée
+        A0 (array): vecteur d'activation de la couche cachée
+        Z1 (array): vecteurs des arguments de la couche de sortie
+        A1 (array): vecteurs d'activation de la couche de sortie
+        data (array): les pixels pour une seule image
+        poids (array): poids et biais [W0, W1, b0, b1]
+
     Returns:
-        dJdW0, dJdb0, dJdW1, dJdb1 = les différentes dérivées de la fonction de perte
+        array: # Je ne suis pas sûre, les 4 sont des vecteurs?
+        dJdW0, dJdb0, dJdW1, dJdb1 : les différentes dérivées de la fonction de perte
     """
     Y = classe(label)
     delta1 = A1 - Y
     delta0 = der_relu(Z0) * np.dot(poids[1],delta1)
-    
+
     dJdW0 = np.dot(data[:,None], delta0[None,:])
     dJdW1 = np.dot(A0[:,None], delta1[None,:])
-    
+
     dJdb0 = delta0
     dJdb1 = delta1
-    
-    return dJdW0, dJdb0, dJdW1, dJdb1
 
+    return dJdW0, dJdb0, dJdW1, dJdb1
 
 
 def actualisation(lambda_, dJdW0_L, dJdb0_L, dJdW1_L, dJdb1_L, poids):
     """
     Actualise les poids, avec un taux d'apprentissage lambda_ donné
-    
+
+    Args:
+        lambda_ (float): taux d'apprentissage
+        dJdW0_L (list): list avec les différents dérivées de W0
+        dJdb0_L (list): list avec les différents dérivées de b0
+        dJdW1_L (lis): list avec les différents dérivées de w1
+        dJdb1_L (list): list avec les différents dérivées de b1
+        poids (array): poids et biais [W0, W1, b0, b1]
+
+    Returns:
+        float: nouveaux poids et biais
     """
     ncouples = len(dJdW0_L) # à tester
-    
+
     # On actualise les poids et biais
     W1 = poids[1] - lambda_/ncouples*np.sum(dJdW1_L, axis = 0)
     W0 = poids[0] - lambda_/ncouples*np.sum(dJdW0_L, axis = 0)
@@ -146,14 +185,27 @@ def actualisation(lambda_, dJdW0_L, dJdb0_L, dJdW1_L, dJdb1_L, poids):
 def proba_max(probas):
     """
     Retourne la position de la probabilité maximum
+
+    Args:
+        probas (array): Vecteur des probabilités
+
+    Returns:
+        float: Position de la probabilité la plus haute
     """
     return np.argmax(probas)
-        
-        
+
+
 def taux_succes(predictions, attendues):
     """
     Retourne le taux de succès d'images trouvées, pour une itération complète
     Le taux est normalisé à 1
+
+    Args:
+        predictions (array): chiffres predites
+        attendues (array): chiffres attendues
+
+    Returns:
+        float: taux de succès
     """
     taux = np.sum(predictions==attendues)/np.size(predictions)
     return taux
@@ -162,15 +214,17 @@ def taux_succes(predictions, attendues):
 def entrainement(X_train, Y_train, n_iterations, lambda_, poids=None, test = False):
     """
     Algorithme d'entrainement du réseau de neuronese
-    Inputs:
-        X_train, Y_train = les images/classes d'entraînement
-        n_itérations = le nombre d'itérations (=époques)
-        lambda_ = le taux d'apprentissage (float)
-        poids = poids initiaux
-        test = booléen permettant de réaliser ou pas l'analyse pour le fichier tests_neurones.py
+
+    Args:
+        X_train (array): les images d'entraînement
+        Y_train (array): les classes d'entraînement
+        n_iterations (int): nombre d'itérations, époques
+        lambda_ (float): taux d'apprentissage
+        poids (array, optional): poids et biais initiaux. Defaults to None.
+        test (bool, optional): permettant de réaliser ou pas l'analyse. Defaults to False.
+
     Returns:
-        W0, W1, b0, b1 = les poids et biais finaux
-        
+        floats: poids et biais finaux
     """
     n_images = len(Y_train)         # correspond au nombre d'image sur lequel on s'entraine
     npixel = X_train[0].size   		# nombre de pixels dans 1 image
@@ -182,13 +236,13 @@ def entrainement(X_train, Y_train, n_iterations, lambda_, poids=None, test = Fal
         # on initialise, même pour toutes les images
         W0, W1, b0, b1 = initialise(npixel, ncouche, nsortie)
         poids = [W0, W1, b0, b1]
-        
+
     for i in range(n_iterations):
         dJdW0_L = []
         dJdb0_L = []
         dJdW1_L = []
         dJdb1_L = []
-        
+
         predictions = [] # le vecteur avec toutes les valeurs trouvées
         for j in range(n_images):
             xi = X_train[j]   # pixels de l'image actuelle
@@ -203,27 +257,29 @@ def entrainement(X_train, Y_train, n_iterations, lambda_, poids=None, test = Fal
 
         W0, W1, b0, b1 = actualisation(lambda_, dJdW0_L, dJdb0_L, dJdW1_L, dJdb1_L, poids) # avec les nouvelles listes de dJ, on actualise les poids
         poids = [W0, W1, b0, b1]
-        
+
         if test :
-        	taux.append(taux_succes(predictions, attendu))
-        	if i%10==0:
-        		print(taux_succes(predictions, attendu))
+            taux.append(taux_succes(predictions, attendu))
+            if i%10==0:
+                print(taux_succes(predictions, attendu))
     if test:
-    	pathfiles = "files/" 
-    	np.save(pathfiles + f"{int(lambda_)}taux", taux) 
-    	print("File saved, path : " + pathfiles + f"{int(lambda_)}taux")      
+        pathfiles = "files/"
+        np.save(pathfiles + f"{int(lambda_)}taux", taux)
+        print("File saved, path : " + pathfiles + f"{int(lambda_)}taux")
     return  W0, W1, b0, b1
-    
 
 
 def validation(X_dev, Y_dev, poids):
     """
     Retourne la classe prédite pour les images de l'échantillon de validation
-    Args :
-        X_dev, Y_dev = les images/classes de validation
-        poids = poids calculés par l'algorithme d'entraînement
-    Returns :
-        le taux de succès (float)
+
+    Args:
+        X_dev (array): les images de validation
+        Y_dev (array): les classes de validation
+        poids (array): poids et biais
+
+    Returns:
+        float: taux de succès
     """
     n_images = len(Y_dev)
     predictions = []
@@ -232,18 +288,3 @@ def validation(X_dev, Y_dev, poids):
         Z0, A0, Z1, A1 = prop_avant(xi, poids)
         predictions.append(proba_max(A1))
     return taux_succes(predictions, Y_dev)
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
--- tests_neurones.py
+++ tests_neurones.py
@@ -1,7 +1,6 @@
 import neurons as n
 import numpy as np
 import matplotlib.pyplot as plt
-
 import style
 
 path2plot = "plots/"
@@ -23,6 +22,7 @@ ncouche = 10
 nsortie = 10
 
 W0, W1, b0, b1 = n.initialise(npixel, ncouche, nsortie)
+print("\nTEST FONCTION INITIALISATION")
 print("Shape matrice poids W0", np.shape(W0), " elle doit être (784, 10)")
 print("Shape matrice poids W1", np.shape(W1), " elle doit être (10, 10)")
 print("Shape matrice biais b0", np.shape(b0), " elle doit être (10,)")
@@ -30,71 +30,73 @@ print("Shape matrice biais b1", np.shape(b1), " elle doit être (10,)")
 
 Z = np.arange(-10,10,1)
 
-## RELU
+# Test RELU
 
 fig, ax = plt.subplots()
-ax.plot(Z, n.relu(Z), c='b', ls='-', label="relu")
-
+ax.plot(Z, n.relu(Z), c='b', ls='-')
 ax.set_xlabel("Z")
 ax.set_ylabel("relu")
-#ax.set_title("Profil amas sans structure qui fusionne")
+ax.set_title("Fonction ReLu")
 plotnom = path2plot + 'relu.pdf'
-ax.legend()
 plt.savefig(plotnom)
-#plt.show()
 
-## Derivée RELU
+
+# Test Derivée RELU
 
 fig, ax = plt.subplots()
-ax.plot(Z, n.der_relu(Z), c='b', ls='-', label="der_relu")
+ax.plot(Z, n.der_relu(Z), c='b', ls='-')
 
 ax.set_xlabel("Z")
 ax.set_ylabel("der_relu")
-#ax.set_title("Profil amas sans structure qui fusionne")
+ax.set_title("Derivée fonction ReLu")
 plotnom = path2plot + 'der_relu.pdf'
-ax.legend()
 plt.savefig(plotnom)
-#plt.show()
 
-## SIGMA
+
+# Test Softmax
 
 fig, ax = plt.subplots()
-ax.plot(Z, n.softmax(Z), c='b', ls='-', label="softmax")
+ax.plot(Z, n.softmax(Z), c='b', ls='-')
 
 ax.set_xlabel("Z")
 ax.set_ylabel("softmax")
-#ax.set_title("Profil amas sans structure qui fusionne")
+ax.set_title("Fonction Softmax")
 plotnom = path2plot + 'softmax.pdf'
 ax.legend()
 plt.savefig(plotnom)
-#plt.show()
 
-## Propagation avant
+
+### TEST FONCTIONS PROPAGATION AVANT ###
+
 poids = [W0, W1, b0, b1]
 Z0, A0, Z1, A1 = n.prop_avant(n.X_dev[0], poids) # X_dev[0] pour tester juste la première image.
+print("\nTEST FONCTION PROPAGATION AVANT")
 print("Shape matrice Z0", np.shape(Z0), " elle doit être (10,)")
 print("Shape matrice A0", np.shape(A0), " elle doit être (10,)")
 print("Shape matrice Z1", np.shape(Z1), " elle doit être (10,)")
 print("Shape matrice A1", np.shape(A1), " elle doit être (10,)")
 
-## Test Classe:
+# Test Classe:
 
 label = 7
+print("\nTEST FONCTION CLASSE")
 print("Classe correspondant au chiffre", label, "est :", n.classe(7))
 
-## Propagation arriere
+### TEST FONCTIONS PROPAGATION ARRIÈRE ###
 
 dJdW0, dJdb0, dJdW1, dJdb1 = n.prop_arriere(label, Z0, A0, Z1, A1, n.X_dev[0], poids) # X_dev[0] pour tester juste la première image.
+print("\nTEST PROPAGATION ARRIÈRE")
 print("Shape matrice dJdW0", np.shape(dJdW0), " elle doit être (784,10)")
 print("Shape matrice dJdb0", np.shape(dJdb0), " elle doit être (10,)")
 print("Shape matrice dJdW1", np.shape(dJdW1), " elle doit être (10,10)")
 print("Shape matrice dJdb1", np.shape(dJdb1), " elle doit être (10,)")
 
 
-# Test de la fonction 'taux de succes':
+# Test de la fonction taux de succes:
 
 a = np.array([1,2,3,4])
 b = np.array([1,3,5,4])
+print("\nTEST FONCTION TAUX DE SUCCES")
 print("taux attendu: 0.5, taux de succes trouvé:", n.taux_succes(a,b))
 
 a = np.array([1,2,3,4])
@@ -102,8 +104,10 @@ b = np.array([1,2,5,4])
 print("taux attendu: 0.75, taux de succes trouvé:", n.taux_succes(a,b))
 
 
-# Test de l'entrainement:
+### ENTRAINEMENT ###
 
+# Premier entrainement avec lambda = 1. :
+print("\nPREMIER ENTRAINEMENT")
 n_iterations = 100
 lambda_ = 1.0
 
@@ -115,18 +119,17 @@ np.save(pathfiles + "b0", b0)
 np.save(pathfiles + "b1", b1)
 
 
-# Test du second entrainement avec lambda = 0.1 :
-
+# Second entrainement avec lambda = 0.1 :
+print("\nSECOND ENTRAINEMENT")
 W0 = np.load(pathfiles + "W0.npy")
 W1 = np.load(pathfiles + "W1.npy")
 b0 = np.load(pathfiles + "b0.npy")
 b1 = np.load(pathfiles + "b1.npy")
 
 n_iterations = 100
-
 lambda_ = 0.1
-poids = [W0, W1, b0, b1]
 
+poids = [W0, W1, b0, b1]
 
 W0_01, W1_01, b0_01, b1_01 = n.entrainement(n.X_train, n.Y_train, n_iterations, lambda_, poids, test = True)
 
@@ -136,7 +139,8 @@ np.save(pathfiles + "b0_01", b0_01)
 np.save(pathfiles + "b1_01", b1_01)
 
 
-# Calcule les taux de succès de l'échantillon de validation :
+# Calcule des taux de succès de l'échantillon de validation :
+
 # Pour le premier entrainement:
 
 W0 = np.load(pathfiles + "W0.npy")
@@ -146,7 +150,8 @@ b1 = np.load(pathfiles + "b1.npy")
 poids = [W0, W1, b0, b1]
 
 taux_succes = n.validation(n.X_dev, n.Y_dev, poids)
-print("Le taux de succès obtenus après un entrainement de 100 itérations à lambda=1:", taux_succes)
+print("\nTAUX PREMIER ENTRAINEMENT")
+print("Le taux de succès obtenus après un entrainement de 100 itérations à lambda = 1:", taux_succes)
 
 # Pour le second:
 
@@ -156,12 +161,12 @@ b0_01 = np.load(pathfiles + "b0_01.npy")
 b1_01 = np.load(pathfiles + "b1_01.npy")
 poids_01 = [W0_01, W1_01, b0_01, b1_01]
 
-
 taux_succes_01 = n.validation(n.X_dev, n.Y_dev, poids_01)
+print("\nTAUX SECOND ENTRAINEMENT")
 print("Le taux de succès obtenus après un second entrainement de 100 itérations à lambda=0.1:", taux_succes_01)
 
 
-# Plot de l'évolution du taux de succès pour les deux taux d'apprentissage 
+# Plot de l'évolution du taux de succès pour les deux taux d'apprentissage
 
 taux0 = np.load(pathfiles + "0taux.npy")
 taux1 = np.load(pathfiles + "1taux.npy")
@@ -173,17 +178,7 @@ ax.plot(taux1, c='b', ls='-', label="1.0")
 ax.plot(taux0, c='r', ls='-', label="0.1")
 ax.set_xlabel("Epoch")
 ax.set_ylabel("Taux de succès")
-#ax.set_title("Evolution du taux de succès pour deux taux d'apprentissage")
+ax.set_title("Evolution du taux de succès pour deux taux d'apprentissage")
 plotnom = path2plot + 'taux_succes_test.pdf'
 ax.legend()
 plt.savefig(plotnom)
-#plt.show()
-
-
-
-
-
-
-
-
-
